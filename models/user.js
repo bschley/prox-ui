c448580d8d06485154c2fed010f1e230abbb087c
bschley <3937642+bschley@users.noreply.github.com>
2023-02-09 20:58:03 +0100

    format code

--- models/user.js
+++ models/user.js
@@ -1,38 +1,42 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../sequelize.js";
 
-const user = sequelize.define('user', {
-  id: {
-    type: DataTypes.UUIDV4,
-    primaryKey: true
+const user = sequelize.define(
+  "user",
+  {
+    id: {
+      type: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    apiToken: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    tokenSecret: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    ugid: {
+      type: DataTypes.STRING,
+      defaultValue: "NA",
+    },
+    role: {
+      type: DataTypes.STRING,
+      defaultValue: "NA",
+    },
   },
-  userName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  apiToken: {
-    type: DataTypes.STRING,
-    unique: true
-  },
-  tokenSecret: { 
-    type: DataTypes.STRING,
-    unique: true
-  },
-  ugid: {
-    type: DataTypes.STRING,
-    defaultValue: 'NA',
-  },
-  role: {
-    type: DataTypes.STRING,
-    defaultValue: 'NA',
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
 export default user;
